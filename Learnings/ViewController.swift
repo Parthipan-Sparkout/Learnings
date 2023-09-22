c02775632d4a37af6e648794465c5059c6a18766
Parthipan <parthiban@sparkouttech.com>
2023-09-22 11:13:08 +0530

    keychain sharing implementation and autofill credential

---
+++ Learnings/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Learnings
+//
+//  Created by Hxtreme on 21/09/23.
+//
+
+import UIKit
+import Security
+import SwiftKeychainWrapper
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        var domain = "app.test"
+        /* let username: String = username.text ?? ""
+        let password = passwordField.text?.data(using: .utf8)!
+
+        // Set attributes
+        let attributes: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: username ?? "john",
+            kSecValueData as String: password ?? "test",
+        ]
+
+        
+        SecAddSharedWebCredential("www.example.com" as CFString, username as CFString, passwordField.text! as CFString) { error in
+            if error != nil {
+             print("error occured")
+              return
+            }
+
+            // The credentials have been successfully saved.
+        } */
+     let keyChain = KeychainWrapper(serviceName: "test", accessGroup: "6CWF679Z8N.com.sparkout.test")
+       let success = keyChain.set("Parthipan", forKey: "name")
+        keyChain.set("Jai", forKey: "subname")
+        print(success)
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-Learnings-LearningsUITests/Pods-Learnings-LearningsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Learnings_LearningsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Learnings_LearningsUITests
+@end
