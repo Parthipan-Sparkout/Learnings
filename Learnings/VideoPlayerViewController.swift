3b7d19cf9359f654ffa05bb960633bfa0e48b413
Parthipan <parthiban@sparkouttech.com>
2023-10-05 18:58:35 +0530

    seek slider, time added

--- Learnings/VideoPlayerViewController.swift
+++ Learnings/VideoPlayerViewController.swift
@@ -14,19 +14,47 @@ class VideoPlayerViewController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var activityLoader: UIActivityIndicatorView!
-    
-    var player: AVPlayer?
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblTotalTime: UILabel!
+    @IBOutlet weak var seekSlider: UISlider! {
+        didSet {
+            self.seekSlider.addTarget(self, action: #selector(onTapToSlide), for: .valueChanged)
+        }
+    }
+    private var timeObserver : Any? = nil
+    private var isThumbSeek : Bool = false
+    private var player: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
-        playVideo()
+                lblTotalTime.textColor = .white
+        lblCurrentTime.textColor = .white
     }
     
-    func setupView(){
+    func setupView() {
         playButton.layer.cornerRadius = playButton.frame.size.width / 2
         playButton.isHidden = true
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        if player == nil {
+            setupView()
+            playVideo()
+            setObserverToPlayer()
+            videoView.bringSubviewToFront(lblCurrentTime)
+            videoView.bringSubviewToFront(lblTotalTime)
+            videoView.bringSubviewToFront(seekSlider)
+        }
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+
+//        let vc = AVPlayerViewController()
+//        vc.player = player
+//        self.present(vc, animated: true)
+//        vc.player?.play()
+//    }
     
     func playVideo() {
         let videoURL = URL(string: "https://static.pexels.com/lib/videos/free-videos.mp4")
@@ -52,6 +80,11 @@ class VideoPlayerViewController: UIViewController {
                     }
                 }
             }
+            if newStatus == .paused {
+                self.playButton.isHidden = false
+            } else {
+                self.playButton.isHidden = true
+            }
         }
     }
     
@@ -71,7 +104,70 @@ class VideoPlayerViewController: UIViewController {
             pauseButton.isHidden = true
         }
     }
+}
 
+// Slider actions with time label
+extension VideoPlayerViewController {
+   
+    private func setObserverToPlayer() {
+        let interval = CMTime(seconds: 0.3, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        timeObserver = player?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main, using: { elapsed in
+            self.updatePlayerTime()
+        })
+    }
+    
+    private func updatePlayerTime() {
+        guard let currentTime = self.player?.currentTime() else { return }
+        guard let duration = self.player?.currentItem?.duration else { return }
+        
+        let currentTimeInSecond = CMTimeGetSeconds(currentTime)
+        let durationTimeInSecond = CMTimeGetSeconds(duration)
+        
+        if self.isThumbSeek == false {
+            self.seekSlider.value = Float(currentTimeInSecond/durationTimeInSecond)
+        }
+        
+        let value = Float64(self.seekSlider.value) * CMTimeGetSeconds(duration)
+        
+        var hours = value / 3600
+        var mins =  (value / 60).truncatingRemainder(dividingBy: 60)
+        var secs = value.truncatingRemainder(dividingBy: 60)
+        var timeformatter = NumberFormatter()
+        timeformatter.minimumIntegerDigits = 2
+        timeformatter.minimumFractionDigits = 0
+        timeformatter.roundingMode = .down
+        guard let hoursStr = timeformatter.string(from: NSNumber(value: hours)), let minsStr = timeformatter.string(from: NSNumber(value: mins)), let secsStr = timeformatter.string(from: NSNumber(value: secs)) else {
+            return
+        }
+        self.lblCurrentTime.text = "\(hoursStr):\(minsStr):\(secsStr)"
+        
+        hours = durationTimeInSecond / 3600
+        mins = (durationTimeInSecond / 60).truncatingRemainder(dividingBy: 60)
+        secs = durationTimeInSecond.truncatingRemainder(dividingBy: 60)
+        timeformatter = NumberFormatter()
+        timeformatter.minimumIntegerDigits = 2
+        timeformatter.minimumFractionDigits = 0
+        timeformatter.roundingMode = .down
+        guard let hoursStr = timeformatter.string(from: NSNumber(value: hours)), let minsStr = timeformatter.string(from: NSNumber(value: mins)), let secsStr = timeformatter.string(from: NSNumber(value: secs)) else {
+            return
+        }
+        self.lblTotalTime.text = "\(hoursStr):\(minsStr):\(secsStr)"
+    }
+    
+   
+    @objc private func onTapToSlide() {
+        self.isThumbSeek = true
+        guard let duration = self.player?.currentItem?.duration else { return }
+        let value = Float64(self.seekSlider.value) * CMTimeGetSeconds(duration)
+        if value.isNaN == false {
+            let seekTime = CMTime(value: CMTimeValue(value), timescale: 1)
+            self.player?.seek(to: seekTime, completionHandler: { completed in
+                if completed {
+                    self.isThumbSeek = false
+                }
+            })
+        }
+    }
 }
 
 extension AVPlayer {
